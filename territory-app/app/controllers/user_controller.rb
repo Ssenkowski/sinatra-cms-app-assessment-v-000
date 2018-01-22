166ef627765f45565f763b0554d3dd56d6f38fc1
Steven Senkowski <scsenkowski@gmail.com>
2018-01-22 23:35:36 +0000

    created get & post /login & get /logout

--- territory-app/app/controllers/user_controller.rb
+++ territory-app/app/controllers/user_controller.rb
@@ -22,4 +22,31 @@ class UserController < ApplicationController
         redirect '/signup'
       end
     end
+
+    get '/login' do
+      if logged_in?
+        redirect '/territories'
+      else
+        erb :'users/login'
+      end
+    end
+
+    post '/login' do
+      @user = User.find_by(username: params[:username])
+      @user.password = 'password'
+      @user.password_confirmation = 'password'
+      @user.save
+      if @user && @user.authenticate('password')
+        session[:user_id] = @user.id
+        redirect '/territories'
+      else
+        redirect '/login'
+      end
+    end
+
+    get '/logout' do
+      session.clear
+      redirect '/login'
+    end
+    
 end
