3e1eaa5223f9614083425a10abe31067f815d46c
Steven Senkowski <scsenkowski@gmail.com>
2018-01-23 00:03:02 +0000

    creating restful territory get, post and patch routes

--- territory-app/app/controllers/territory_controller.rb
+++ territory-app/app/controllers/territory_controller.rb
@@ -11,9 +11,29 @@ class TerritoryController < ApplicationController
     erb :'/territories/new'
   end
 
+  post '/territories/new' do
+
+    erb :'/territories/new'
+  end
+
+  get "/territories/:id" do
+
+    redirect "/territories/show_territory"
+  end
+
+  patch '/territories/:id/edit' do
+
+    redirect "/territories/#{@territory.id}"
+  end
+
   get '/territories/sign_in_or_out' do
 
-    erb :"/territories/sign_in_or_out"
+     :"/territories/sign_in_or_out"
+  end
+
+  post '/territories/sign_in_or_out' do
+
+    redirect :"/users/show_user"
   end
 
 end
